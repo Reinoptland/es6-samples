fd3a3cb9e3ce8d2dc8156e3fae3b32a9e14ae893
Reinoptland <reinoptland@gmail.com>
2021-03-04 14:39:45 +0100

    Can we have drinks dry

--- canWeHaveDrinks.js
+++ canWeHaveDrinks.js
@@ -1,5 +1,5 @@
-const cafeOpen = true;
-const age = 16;
+const cafeOpen = false;
+const age = 33;
 const hasMoney = true;
 
 let output = "";
@@ -13,4 +13,24 @@ if (age < 18) {
   output = "Drink responsibly!";
 }
 
-console.log(output);
+function canWeHaveDrinks(cafeOpen, age, hasMoney) {
+  switch (true) {
+    case age < 18: // true of false
+      return "You are too young!";
+    case !cafeOpen:
+      return "Wait until te cafe is open";
+    case !hasMoney: // !hasMany -> true
+      return "I guess you can have water!";
+
+    default:
+      return "Drink responsibly!";
+  }
+}
+
+console.log(canWeHaveDrinks(true, 33, false));
+
+if (age > 18 && hasMoney && cafeOpen) {
+  console.log("Drink responsibly");
+} else {
+  console.log("NOPE!"); // :( ..?? Why??
+}
