5f284cd8e84d3448237aa1030e3166b4decf6fb2
Reinoptland <reinoptland@gmail.com>
2021-03-04 15:04:54 +0100

    Desctructure from a require statement

--- destructure-require.js
+++ destructure-require.js
@@ -1,14 +1,26 @@
-const readline = require("readline-sync");
+// const readline = require("readline-sync");
+// const { question, keyInSelect } = readline;
+const someModule = {
+  question: function () {
+    console.log("What is love?");
+  },
+};
 
-const feeling = readline.question("How do you feel about JS basics? ");
+const { question, keyInSelect } = require("readline-sync");
+const { question: whatIsLove } = someModule;
+
+whatIsLove();
+
+const feeling = question("How do you feel about JS basics? ");
+console.log("Are you ready for React");
 const options = [
   "Yes",
   "I don't know how to react...",
   "No, but are we ever truly ready?",
 ];
-const choice = readline.keyInSelect(options);
+const choice = keyInSelect(options);
 
-const anythingToAdd = readline.question("Any thoughts you want to add?");
+const anythingToAdd = question("Any thoughts you want to add?");
 
 console.log(`
 You feel: ${feeling}
